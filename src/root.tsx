982befc227098b8654a73ecef0fcfa7acfe1fff5
thejackshelton <me@jackshelton.com>
2025-03-13 22:05:01 -0500

    simplify repro more

--- src/root.tsx
+++ src/root.tsx
@@ -4,34 +4,28 @@ import { isDev } from "@qwik.dev/core/build";
 import { RouterHead } from "./docs-widgets/router-head/router-head";
 
 export default component$(() => {
-  /**
-   * The root of a QwikCity site always start with the <QwikCityProvider> component,
-   * immediately followed by the document's <head> and <body>.
-   *
-   * Don't remove the `<head>` and `<body>` elements.
-   */
+	/**
+	 * The root of a QwikCity site always start with the <QwikCityProvider> component,
+	 * immediately followed by the document's <head> and <body>.
+	 *
+	 * Don't remove the `<head>` and `<body>` elements.
+	 */
 
-  return (
-    <QwikRouterProvider>
-      <head>
-        <meta charset="utf-8" />
-        {!isDev && (
-          <link rel="manifest" href={`${import.meta.env.BASE_URL}manifest.json`} />
-        )}
-        <RouterHead />
-      </head>
-      <body lang="en">
-        <RouterOutlet />
-        <script
-          dangerouslySetInnerHTML={`
-            window.addEventListener('initPagefind', async () => {
-              const pagefind = await import("/pagefind/pagefind.js");
-              await pagefind.init();
-              window.pagefind = pagefind;
-            });
-          `}
-        />
-      </body>
-    </QwikRouterProvider>
-  );
+	return (
+		<QwikRouterProvider>
+			<head>
+				<meta charset="utf-8" />
+				{!isDev && (
+					<link
+						rel="manifest"
+						href={`${import.meta.env.BASE_URL}manifest.json`}
+					/>
+				)}
+				<RouterHead />
+			</head>
+			<body lang="en">
+				<RouterOutlet />
+			</body>
+		</QwikRouterProvider>
+	);
 });
